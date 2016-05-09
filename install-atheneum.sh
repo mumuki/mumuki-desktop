ac24eb370729152a6d09d482558b7935d198a4f1
Rodrigo Alfonso <rodri042@gmail.com>
2016-05-09 02:07:36 -0300

    Fixing gpg import for rvm and vm size

--- install-atheneum.sh
+++ install-atheneum.sh
@@ -4,7 +4,8 @@ REPO="https://github.com/rodri042/mumuki-atheneum"
 cd
 
 # Install rvm
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -#LO https://rvm.io/mpapis.asc
+gpg --import mpapis.asc
 curl -sSL https://get.rvm.io | bash -s stable
 . /home/vagrant/.rvm/scripts/rvm
 
