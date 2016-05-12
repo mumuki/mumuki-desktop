aac18aa5390826315a9acd2ae7dd3e1cb191794b
Rodrigo Alfonso <rodri042@gmail.com>
2016-05-12 04:01:21 -0300

    Refactor of install-runner and support to update the sql automatically

--- install-runner.rb
+++ install-runner.rb
@@ -1,16 +1,22 @@
 #!/usr/bin/env ruby
 
 require "optparse"
-load "languages_index.rb"
+load "languages_local.rb"
 load "languages_sql.rb"
 
-INSTALL_SCRIPT = ".mumuki-offline/install.sh"
+INSTALL_SCRIPT = <<-EOF
+  git submodule update --init --recursive
+  bundle install
+EOF
+
+$local = LocalIndex.new
+$sql = SqlIndex.new
 
 # Installs a language on the machine
 # (it must exist in the database)
 def install(language_name)
-  get_language(language_name) { |language, repo_url|
-    if is_installed language_name
+  $sql.get_language(language_name) { |language, repo_url|
+    if $local.is_installed? language_name
       abort "The language '#{language_name} is already installed."
     end
 
@@ -24,21 +30,16 @@ def install(language_name)
       fi
 
       cd #{language_name}
-      if [ ! -f #{INSTALL_SCRIPT} ]; then
-        echo "Installer not found."
-        exit 1
-      fi
-
-      bash #{INSTALL_SCRIPT}
+      #{INSTALL_SCRIPT}
       if [ $? -ne 0 ]; then
-        echo "The installer has exploded!"
+        echo "The install command has exploded!"
         exit 1
       fi
     EOF
     if not success then abort end
 
-    port = add_language_to_index language_name
-    add_language_to_database language["id"], port
+    port = $local.add_language! language_name
+    update
 
     puts "The language '#{language_name}' was installed and now it runs on port #{port}!"
   }
@@ -46,8 +47,8 @@ end
 
 # Uninstalls a language from the machine
 def uninstall(language_name)
-  get_language(language_name) { |language, repo_url|
-    if not is_installed language_name
+  $sql.get_language(language_name) { |language, repo_url|
+    if not $local.is_installed? language_name
       abort "The language '#{language_name} is not installed yet."
     end
 
@@ -58,10 +59,16 @@ def uninstall(language_name)
     EOF
     if not success then abort end
 
-    remove_language_from_index language_name
+    $local.remove_language! language_name
+    update
 
     puts "The language '#{language_name}' was uninstalled!"
   }
+end
+
+# Updates the languages sql table.
+def update
+  $sql.update_languages_table $local.info
 end
 
 # Displays the help on stdout
@@ -69,6 +76,7 @@ def show_help
   puts "Examples:"
   puts "./install-runner.rb --install gobstones"
   puts "./install-runner.rb --uninstall haskell"
+  puts "./install-runner.rb --update"
   abort
 end
 
@@ -78,4 +86,5 @@ OptionParser.new do |opt|
   opt.on("--help") { show_help }
   opt.on("--install LANGUAGE") { |it| install it }
   opt.on("--uninstall LANGUAGE") { |it| uninstall it }
+  opt.on("--update") { update }
 end.parse!
--- languages_sql.rb
+++ languages_sql.rb
@@ -1,36 +1,39 @@
 require "pg"
 load "languages_sql_repos.rb"
 
-# Connects to the db
-def connect
-  PG.connect(
-    host: "localhost",
-    dbname: "mumuki_development",
-    user: "mumuki",
-    password: "mumuki"
-  )
-end
+class SqlIndex
+  def initialize
+    @connection = PG.connect(
+      host: "localhost",
+      dbname: "mumuki_development",
+      user: "mumuki",
+      password: "mumuki"
+    )
+  end
 
-# Retrieves a language from the database
-def get_language(language_name)
-  connect
-    .exec("select * from languages;") { |languages|
-      language = languages.find { |it| it["name"] == language_name }
-      repo_url = repos[language_name]
+  # Retrieves a language from the database
+  def get_language(language_name)
+    @connection
+      .exec("select * from languages;") { |languages|
+        language = languages.find { |it| it["name"] == language_name }
+        repo_url = repos[language_name]
 
-      if language.nil?
-        abort "The language '#{language_name}' wasn't found. Did you run `rake db:seed`?"
-      end
+        if language.nil?
+          abort "The language '#{language_name}' wasn't found. Did you run `rake db:seed`?"
+        end
 
-      if repo_url.nil?
-        abort "Missing repository url for '#{language_name}'."
-      end
+        if repo_url.nil?
+          abort "Missing repository url for '#{language_name}'."
+        end
 
-      yield language, repo_url
-    }
-end
+        yield language, repo_url
+      }
+  end
 
-# Updates the url of a runner with the port
-def add_language_to_database(id, port)
-  connect.exec "update languages set test_runner_url='http://localhost:#{port}' where id = #{id}"
+  # Updates the url of the runners with its assigned ports
+  def update_languages_table(info)
+    info["languages"].each do |language|
+      @connection.exec "update languages set test_runner_url='http://localhost:#{language['port']}' where name = '#{language['name']}'"
+    end
+  end
 end
