3b1a5747dc4e6cfed8573fb9f6dc90cecbe209f8
Rodrigo Alfonso <rodri042@gmail.com>
2016-05-17 02:37:26 -0300

    Omit first newline in the log

--- loader/MumukiLoader.Core/Helpers/LoggerExtensions.cs
+++ loader/MumukiLoader.Core/Helpers/LoggerExtensions.cs
@@ -1,10 +1,14 @@
-﻿namespace MumukiLoader.Core.Helpers {
+﻿using System;
+
+namespace MumukiLoader.Core.Helpers {
 	public static class LoggerExtensions {
 		/// <summary>
 		/// Adds a line to the logger.
 		/// </summary>
 		public static void AddLine(this Logger self, string line) {
-			self.Text = $"{self.Text}\r\n{line}";
+			self.Text = self.Text.Length != 0
+				? $"{self.Text}\r\n{line}"
+				: line;
 		}
 	}
 }
